a99ade265d39f9d219c27db47d573f97fe985bb6
Natalia Tayoane <nataliatayoane@gmail.com>
2021-06-21 23:07:58 -0300

    Maximum Speed

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -6,6 +6,7 @@ import "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
@@ -13,5 +14,7 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { auth, firebase, database };
