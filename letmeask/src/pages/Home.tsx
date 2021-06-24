8674e8843c744466b8fb690e1221c133027d04a9
Natalia Tayoane <nataliatayoane@gmail.com>
2021-06-24 18:54:21 -0300

    Landing

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -8,10 +8,10 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 
-import '../styles/auth.scss';
-import '../styles/global.scss';
 import { database } from '../services/firebase';
 
+import '../styles/auth.scss';
+import '../styles/global.scss';
 
 export function Home() {
   const history = useHistory();
@@ -35,6 +35,11 @@ export function Home() {
 
     if (!roomRef.exists()) {
       alert('Room does not exist.');
+      return;
+    }
+
+    if (roomRef.val().endedAt) {
+      alert('Room already closed.');
       return;
     }
     history.push(`/rooms/${roomCode}`);
